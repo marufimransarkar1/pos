833e4d34bde5e9df1b71664e557d8738a3b7e484
unknown <marufimran434@gmail.com>
2026-04-13 21:58:04 +0600

    it is final

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-  server: { proxy: { '/api': 'http://localhost:5000' } },
-  // server: { proxy: { '/api': 'https://posbackend-ten.vercel.app' } },
+  // server: { proxy: { '/api': 'http://localhost:5000' } },
+  server: { proxy: { '/api': 'https://posbackend-ten.vercel.app' } },
   build: {
     sourcemap: false,
   },
